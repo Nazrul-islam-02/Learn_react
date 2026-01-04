fbe2d125fa920c1061e33f99723e8d1714593fd9
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-04 09:43:32 +0600

    practris gitignore file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import React_Js_expression from './Components/React_Js_expression';
+
+const App = () => {
+  return (
+    <div>
+      <React_Js_expression></React_Js_expression>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/React_Js_expression.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const React_Js_expression = () => {
+    return (
+        <div className='bg-gray-200 text-center p-4 text-black font-bold underline'>
+            This is Component;
+        </div>
+    );
+};
+
+export default React_Js_expression;
